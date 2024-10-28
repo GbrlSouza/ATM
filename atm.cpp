0284068c85ab071c16260ce960b73cc956c560c4
Gabriel Souza <gabrielsouza_1909@hotmail.com>
2024-10-28 11:55:17 -0300

    Update atm.cpp

--- atm.cpp
+++ atm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> 
 using namespace std;
 
 class Conta {
@@ -9,28 +10,32 @@ public:
     Conta() : saldo(0.0) {}
 
     void depositar(double valor) {
-        saldo += valor;
-        cout << "Depósito de R$ " << valor << " realizado.\n";
+        if (valor > 0) {
+            saldo += valor;
+            cout << "Depósito de R$ " << fixed << setprecision(2) << valor << " realizado.\n";
+        } else {
+            cout << "Valor de depósito inválido.\n";
+        }
     }
 
     void sacar(double valor) {
         if (valor <= saldo) {
             saldo -= valor;
-            cout << "Saque de R$ " << valor << " realizado.\n";
+            cout << "Saque de R$ " << fixed << setprecision(2) << valor << " realizado.\n";
         } else {
             cout << "Saldo insuficiente.\n";
         }
     }
 
     void consultarSaldo() {
-        cout << "Saldo atual: R$ " << saldo << endl;
+        cout << "Saldo atual: R$ " << fixed << setprecision(2) << saldo << endl;
     }
 
     void transferir(Conta &destino, double valor) {
         if (valor <= saldo) {
             saldo -= valor;
             destino.depositar(valor);
-            cout << "Transferência de R$ " << valor << " realizada.\n";
+            cout << "Transferência de R$ " << fixed << setprecision(2) << valor << " realizada.\n";
         } else {
             cout << "Saldo insuficiente para transferência.\n";
         }
@@ -41,7 +46,7 @@ int main() {
     Conta conta1, conta2;
     int opcao;
     double valor;
-    
+
     do {
         cout << "1. Depositar\n2. Sacar\n3. Consultar Saldo\n4. Transferir\n0. Sair\nEscolha: ";
         cin >> opcao;
